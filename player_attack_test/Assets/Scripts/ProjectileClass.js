18b3ef4f79805e230e562df6d4e30f4802e83b51
Hannah <vanha241@student.otago.ac.nz>
2015-01-20 15:01:29 +1300

    added animation test and player attack test. both are pretty average at the mo, just btw

---
+++ player_attack_test/Assets/Scripts/ProjectileClass.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+//this class controls all attacks
+
+//projectile speed
+var speed: float;
+
+//destroy effect
+var DestroyEffect : Transform;
+
+function Update () {
+	//for now the default direction of the projectile will be right
+	transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+   // Check if the game object is visible, if not, destroy self   
+   if(!UtilScript.isVisible(renderer, Camera.main)) {
+      if(DestroyEffect != null) {
+         var destroy = Instantiate(DestroyEffect);
+         destroy.position = transform.position;
+      }
+      Destroy(gameObject);
+   }
+
+}
