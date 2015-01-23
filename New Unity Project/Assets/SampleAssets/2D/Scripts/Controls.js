e03a8672e7d1e89835d4e7af3d861435287651ad
Dylan <Dylan@localhost>
2015-01-23 16:16:35 +1300

    added lighting and stuff pls

--- New Unity Project/Assets/SampleAssets/2D/Scripts/Controls.js
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/Controls.js
@@ -3,35 +3,14 @@
 private var character : Character;
 private var jump : boolean;
 
-//indicating that the player has arrived at a story element
-private var atStory : boolean;
-
-
-
 private function Awake() {
 	character = GetComponent(Character);
 }
-
-//on collision with a trigger
-//function OnTriggerEnter2D(other : Collider2D) {
-   //check the tag of the object the 
-   //player has collided with
-//   if (other.tag == "Story") {
-   // if collided with story object,
-   //set story flag to true
-//   atStory = true;
-//   }
-//}
 
 private function Update() {
 	if (!jump) {
 		jump = Input.GetButtonDown("Jump");
 	}
-	//if in front of a story element stop moving
-	if (atStory) {
-	   character.maxSpeed = 0;
-	}
-	
 	if(Input.GetKeyDown(KeyCode.Alpha1)) {
        // Get players attack component
        // and execute its shoot() method
