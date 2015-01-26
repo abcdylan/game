47174f8f0c4bbc6c1330fba39383f8305a47e03f
Hannah <vanha241@student.otago.ac.nz>
2015-01-26 14:21:32 +1300

    worked on ability pick-ups basic implemetation

--- New Unity Project/Assets/SampleAssets/2D/Scripts/FloatingText.js
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/FloatingText.js
@@ -1,9 +1,31 @@
 ﻿#pragma strict
 
+public var storyPanel : Animator;
+private var isHidden : boolean;
+
+function showStory() {
+   storyPanel.enabled = true;
+   //gameObject.SetActive(true);
+   //isHidden = storyPanel.GetBool("isHidden");
+   storyPanel.SetBool("isHidden", !isHidden);  
+}
+
+function Hide() {
+//   storyPanel.enabled = true;
+//   gameObject.SetActive(false);
+//   isHidden = storyPanel.GetBool("isHidden");
+   storyPanel.SetBool("isHidden", isHidden);  
+}
+
 function Start () {
-
+   var transform : RectTransform = storyPanel.gameObject.transform as RectTransform;
+   var position : Vector2 = transform.anchoredPosition;
+   position.y -= transform.rect.height;
+   transform.anchoredPosition = position; 
+   storyPanel = GetComponent(Animator);
 }
 
-function Update () {
 
-}
+//function Update () {
+
+//}
