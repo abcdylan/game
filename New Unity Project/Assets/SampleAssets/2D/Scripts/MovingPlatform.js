6b1b6ef2fb77cb933d65083eb55185a0a5a5bf76
Hannah <vanha241@student.otago.ac.nz>
2015-01-27 10:30:09 +1300

    removed files that were no longer recognised since they had been moved, and added move platform

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/MovingPlatform.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+public var DestinationSpot : Transform;
+public var OriginSpot : Transform;
+public var speed : float;
+public var Switch : boolean = false;
+
+
+
+function FixedUpdate () {
+   // checking the position of the platform
+   if (transform.position == DestinationSpot.position) {
+      Switch = true;
+   }
+   if (transform.position == OriginSpot.position) {
+      Switch = false;
+   }
+   //if switch then move the platform to origin
+   if (Switch) {
+      transform.position = new Vector3.MoveTowards(transform.position, OriginSpot.position, speed);
+   } else {
+   // otherwise move platform to destination
+      transform.position = new Vector3.MoveTowards(transform.position, DestinationSpot.position, speed);
+   }
+      
+}
