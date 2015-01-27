58e4a0500f4cd8ae58608be97b207e8e9a8131fa
Hannah <vanha241@student.otago.ac.nz>
2015-01-27 13:50:12 +1300

    modified showing story panels on trigger enter

--- New Unity Project/Assets/SampleAssets/2D/Scripts/FloatingText.js
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/FloatingText.js
@@ -1,29 +1,33 @@
 ﻿#pragma strict
 
+import UnityEngine.UI;
+
 public var storyPanel : Animator;
+
+var storyText: Text = null;
 private var isHidden : boolean;
 
-function showStory() {
+function showStory() { 
    storyPanel.enabled = true;
-   //gameObject.SetActive(true);
+   gameObject.SetActive(true);
    //isHidden = storyPanel.GetBool("isHidden");
    storyPanel.SetBool("isHidden", !isHidden);  
 }
 
 function Hide() {
-//   storyPanel.enabled = true;
-//   gameObject.SetActive(false);
 //   isHidden = storyPanel.GetBool("isHidden");
-   storyPanel.SetBool("isHidden", isHidden);  
+   storyPanel.SetBool("isHidden", isHidden); 
+   storyPanel.enabled = false;
+   gameObject.SetActive(false); 
 }
 
-function Start () {
-   var transform : RectTransform = storyPanel.gameObject.transform as RectTransform;
-   var position : Vector2 = transform.anchoredPosition;
-   position.y -= transform.rect.height;
-   transform.anchoredPosition = position; 
-   storyPanel = GetComponent(Animator);
-}
+//function Start () {
+   //var transform : RectTransform = storyPanel.gameObject.transform as RectTransform;
+   //var position : Vector2 = transform.anchoredPosition;
+   //position.y -= transform.rect.height;
+   //transform.anchoredPosition = position; 
+   //storyPanel = GetComponent(Animator);
+//}
 
 
 //function Update () {
--- New Unity Project/Assets/SampleAssets/2D/Scripts/StoryController.js
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/StoryController.js
@@ -2,8 +2,10 @@
 
 import UnityEngine.UI;
 
+//reference to UI panel object
 var storyPanel : GameObject;
 
+// reference to panels script object
 private var story : FloatingText;
 
 var TellStory : boolean = false;
@@ -21,6 +23,8 @@ function OnTriggerExit2D (other : Collider2D) {
 }
 
 function Start () {
+   //initialise reference to script object, which is a component of
+   // the panel game object
    story = storyPanel.GetComponent(FloatingText);
    //story.SetActive(false);
    story.Hide();
@@ -32,6 +36,7 @@ function Update () {
       //story.SetActive(true);
       story.showStory();
    } else {
+      //yield WaitForSeconds(5);
       story.Hide();
    }
 }
