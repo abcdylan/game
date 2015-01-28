4a779667a70af3a7d07cdd84abc27c358ddd214b
Hannah <vanha241@student.otago.ac.nz>
2015-01-28 14:06:22 +1300

    adding changes to New Unity Project: addition of Story Manager Box prefab, and child controller script which controls what things are the children of a moving platform

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/ChildController.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(other : Collider2D) {
+  other.transform.parent = gameObject.transform;
+  
+}
+
+function OnTriggerExit2D(other : Collider2D) {
+  other.transform.parent = null;
+  
+}
+
+
+/*function Start () {
+
+}
+
+function Update () {
+
+}*/
