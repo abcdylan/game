48bf4a3c991fba12ca1dbc79ed3c1ad3b29419d3
nmckellow <nmckellow@localhost>
2015-01-29 15:56:37 +1300

    Attempting to upload the Character.js script with working double jump mechanic (if it doesn't work, there has been an error via committing/merging/that shit)

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/MovingPlatform.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+// speed of platform
+var platform : Transform;
+
+var moveSpeed : float;
+
+var Switch : boolean;
+
+
+var startPosition : Transform;
+var endPosition : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function FixedUpdate(){
+if(transform.position == endPosition.position){
+Switch = true;
+}
+if(transform.position == startPosition.position){
+Switch = false;
+}
+if(Switch){
+platform.position = Vector3.MoveTowards(endPosition.position, startPosition.position, moveSpeed);
+} else {
+transform.position = Vector3.MoveTowards(startPosition.position, endPosition.position, moveSpeed);
+}
+}
