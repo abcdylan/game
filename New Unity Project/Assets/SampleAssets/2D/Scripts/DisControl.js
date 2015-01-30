d4e22ada5a3cffe452c9d1b755d2afa54ed3755a
Hannah <vanha241@student.otago.ac.nz>
2015-01-31 11:04:55 +1300

    adding changes made i.e added disppearPlatform and DisControl and HideObjectController and Script. note that these are all terrible and don't work. also deleted scene2 and sdfd scenes

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/DisControl.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+// reference to game object that contains script
+var child : GameObject;
+
+// reference to script of gameObject;
+private var childControl : DisappearPlatform;
+
+function Start () {
+   //initialise reference to script object, which is a component of
+   // the panel game object
+   childControl = child.GetComponent(DisappearPlatform);
+   //child.SetActive(true);
+   
+} 
+
+/*
+function Update() {
+   if(!child.active) {
+      child.ActivateMe();
+   } else {
+      child.DeactivateMe();
+   }  
+}
+*/
+
+function Update () {
+   if(!child.activeSelf) {
+      //child.GetComponent(SpriteRenderer).color.a += 0.2 * Time.deltaTime * 2 ;
+      wait();
+   } /*else {
+     if(child.GetComponent(SpriteRenderer).color.a < 255) {
+	       child.GetComponent(SpriteRenderer).color.a += 0.2 * Time.deltaTime * 2 ;
+     }*/
+}
+
+
+
+function wait() {
+   //WaitForSeconds(5);
+   if (!child.activeSelf) {
+      child.GetComponent(SpriteRenderer).color.a += 0.2 * Time.deltaTime * 2 ;
+      //child.SetActive(true);
+    }
+    child.SetActive(true);
+}
---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/HideObjectController.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var obj : GameObject;
+
+var timer : float;
+
+private var timeLeft : float;
+
+private var objScript : HideObjectScript;
+
+private var isShow : boolean;
+
+function Start () {
+   objScript = obj.GetComponent(HideObjectScript);
+   objScript.Show();
+   isShow = true;
+}
+
+function Update () {
+   if (timeLeft > 0) {
+      timeLeft -= Time.deltaTime;
+   } else {
+      changeState();
+   }
+
+}
+
+function changeState() {
+   if(isShow) {
+      obj.Hide();
+      isShow = false;
+      timeLeft = timer;
+   } else {
+      obj.Show();
+      isShow = true;
+   }
+}
+   
