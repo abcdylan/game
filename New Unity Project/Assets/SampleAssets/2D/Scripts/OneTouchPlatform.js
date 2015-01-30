b4e071f0d1a6e8479e6d7de0f8c31ab09eb5647d
Hannah <vanha241@student.otago.ac.nz>
2015-01-30 14:14:23 +1300

    added one touch platform stuff but it doesn't do anything thought i did this on the last commit, but turns out not

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/OneTouchPlatform.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+// the object you want to be able to respawn that
+// has its own destruct method
+//var cloud : Transform;
+
+var spawnPos : Transform;
+
+//the time until the respawn can occur
+public var timer : float;
+
+//the time left on the timer
+private var timeLeft : float;
+
+// private bool to check if the transform has been destroyed
+private var hasGone : boolean = false;
+
+//per each frame
+function Update() {
+   //check how much time is left on the timer
+   // and decrease that time
+   if(timeLeft > 0) {
+      timeLeft -= Time.deltaTime;
+   }
+   if(timeLeft == 0) {
+      Destroy(gameObject);
+      hasGone = true;
+      spawnCloud();
+   }
+}
+
+// spawning the object we want
+function spawnCloud() {
+   if (hasGone) {
+      var newCloud = Instantiate(transform);
+      newCloud.position = spawnPos.position;
+      timeLeft = timer;
+      hasGone = false;
+   }
+}
