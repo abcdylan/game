84ff0551631fcc09a4bd566fc058b070c6f30634
nmckellow <nmckellow@localhost>
2015-01-30 15:08:47 +1300

    pulling something

---
+++ New Unity Project/Assets/Scripts/PlayerClass.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+// for the purposes of this prototype the plyer is 
+// just there to be a game object that shoots
+
+function Update () {
+    if(Input.GetButton("Jump")) {
+       // Get players attack component
+       // and execute its shoot() method
+       var attack : AttackClass;
+       attack = GetComponent(AttackClass);
+       attack.Shoot();
+       
+    }
+}
