91dc2cddc06a935ee0737e4dcbc473503a3775e6
Hannah <vanha241@student.otago.ac.nz>
2015-01-31 11:04:55 +1300

    adding changes made i.e added disppearPlatform and DisControl and HideObjectController and Script. note that these are all terrible and don't work. also deleted scene2 and sdfd scenes

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/DisappearPlatform.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+//var platform : GameObject;
+
+//public var isActive : boolean = false;
+
+function Update() {
+	if(gameObject.tag == "DisPlatform") {
+		if(gameObject.GetComponent(SpriteRenderer).color.a > 0) {
+			gameObject.GetComponent(SpriteRenderer).color.a -= 0.2 * Time.deltaTime * 2 ;
+		} else {
+		   gameObject.SetActive(false);
+	    }
+     }
+}
+/*
+function ActivateMe() {
+   while (gameObject.GetComponent(SpriteRenderer).color.a < 255){
+      gameObject.GetComponent(SpriteRenderer).color.a += 0.2 * Time.deltaTime * 2 ;
+   }
+   gameObject.SetActive(true);
+}
+
+function DeactivateMe() {
+   while (gameObject.GetComponent(SpriteRenderer).color.a > 0) {
+      gameObject.GetComponent(SpriteRenderer).color.a -= 0.2 * Time.deltaTime * 2 ;
+	} 
+	gameObject.SetActive(false);
+}
+
+
+
+/*
+function Start () {
+
+}
+
+function Update () {
+
+}*/
---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/HideObjectScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+//var platformPrefab : GameObject;
+
+/*
+function Start () {
+
+}*/
+/*
+function Update () {
+
+}*/
+
+function Hide() {
+   gameObject.SetActive(false);
+}
+
+function Show() {
+   gameObject.SetActive(true);
+}
+
