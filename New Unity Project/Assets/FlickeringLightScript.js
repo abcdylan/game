d2aab22d67fba3d2ed05e48d4afa761318484e92
lenzoburger <lenzoburger@localhost>
2015-01-30 13:30:13 +1300

    Finished off buliding Ice level

---
+++ New Unity Project/Assets/FlickeringLightScript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var minFlickerSpeed : float = 0.1;
+var maxFlickerSpeed : float = 1.0;
+
+private function Update() {
+	LightFlicker();
+}  
+
+function LightFlicker() {
+	GetComponent(Light).intensity = 5;
+	yield WaitForSeconds (Random.Range(minFlickerSpeed, maxFlickerSpeed));
+	GetComponent(Light).intensity = 10;
+	yield WaitForSeconds (Random.Range(minFlickerSpeed, maxFlickerSpeed));
+}
---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/MovingPlatform.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+
+public var DestinationSpot : Transform;
+public var OriginSpot : Transform;
+public var speed : float;
+public var Switch : boolean = false;
+
+
+
+function FixedUpdate () {
+   // checking the position of the platform
+   if (transform.position == DestinationSpot.position) {
+      Switch = true;
+   }
+   if (transform.position == OriginSpot.position) {
+      Switch = false;
+   }
+   //if switch then move the platform to origin
+   if (Switch) {
+      transform.position = new Vector3.MoveTowards(transform.position, OriginSpot.position, speed);
+   } else {
+   // otherwise move platform to destination
+      transform.position = new Vector3.MoveTowards(transform.position, DestinationSpot.position, speed);
+   }
+        
+}
+
+function OnDrawGizmos() {
+   Gizmos.color = Color.green;
+   Gizmos.DrawWireCube(OriginSpot.position, transform.localScale);
+   
+   
+   Gizmos.color = Color.red;
+   Gizmos.DrawWireCube(DestinationSpot.position, transform.localScale);
+   
+}
