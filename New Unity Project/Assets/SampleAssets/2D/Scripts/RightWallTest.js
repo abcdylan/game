0bdbee030f4d762d750ae7028babb0c55695e4f6
lenzoburger <lenzoburger@localhost>
2015-01-31 17:26:17 +1300

    somestuff

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/RightWallTest.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+//references wall collider
+var rightWall: EdgeCollider2D;
+
+function Start () {
+
+   //get the width and height of the main camera since it provides us with an easy wall to use
+   var screenW : float = Camera.main.pixelWidth;
+   var screenH : float = Camera.main.pixelHeight;
+	
+   //create an array which will hold 2 Vector2 objects measurements
+   var edgePoints: Vector2[] = new Vector2[2];
+
+   // Convert screen coordinates point (W,0) to world coordinates
+   var rightBottom : Vector3 = Camera.main.ScreenToWorldPoint(new Vector3(screenW, 0f, 0f));      
+   // Convert screen coordinates point (W,H) to world coordinates
+   var rightTop : Vector3 = Camera.main.ScreenToWorldPoint(new Vector3(screenW, screenH, 0f));      
+
+   // Set the two points in the array to screen right bottom
+   // and screen right top points            
+   edgePoints[0] = rightBottom;
+   edgePoints[1] = rightTop;
+
+   // Position the left wall edge collider
+   // at the left edge of the camera
+   rightWall.points = edgePoints;
+}
+
---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/StoryController.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+
+//public var openBox : Animator;
+
+//reference to UI panel object
+var storyPanel : GameObject;
+
+
+var TellStory : boolean = false;
+
+// reference to panels script object
+private var story : FloatingText;
+
+private var isEnter : boolean = false;
+
+function OnTriggerEnter2D (other : Collider2D) {
+    if( other.tag == "Player") {
+       TellStory = true;
+    }
+}
+
+function OnTriggerExit2D (other : Collider2D) {
+   if(other.tag == "Player") {
+      TellStory = false;
+   }
+}
+
+function Start () {
+   //openBox = GetComponent(Animator);
+   //initialise reference to script object, which is a component of
+   // the panel game object
+   story = storyPanel.GetComponent(FloatingText);
+   //story.SetActive(false);
+   story.Hide();
+         
+}
+
+function Update () {
+   if (TellStory) {
+      //story.SetActive(true);
+      story.showStory();
+      //isEnter = true;
+      
+   } else {
+      //yield WaitForSeconds(5);
+      story.Hide();
+   }
+}
