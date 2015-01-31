bc12908f767f9e3cd347af0e632126caee6dfd0a
Hannah <vanha241@student.otago.ac.nz>
2015-01-31 13:58:14 +1300

    added respawn script and gave respawn positions in ice level demo

---
+++ New Unity Project/Assets/SampleAssets/2D/Scripts/RespawnPlayer.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+//var Player : GameObject;
+
+var spawnPoint : Transform;
+
+function OnTriggerEnter2D(other : Collider2D) {
+   //Destroy(other.gameObject);
+   //var P : GameObject;
+   //var P = Instantiate(Player);
+   other.transform.position = spawnPoint.position;
+   //var cam = camera.main.getComponent(CameraController);
+   //camera.player = P.transform;
+   
+}
+
+/*
+function Start () {
+
+}
+
+function Update () {
+
+}*/
