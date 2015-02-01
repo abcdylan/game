3f2ecd247f759da8bf006b2e2861133105756adb
Hannah <vanha241@student.otago.ac.nz>
2015-02-02 12:46:20 +1300

    edited the story Box prefab

--- New Unity Project/Assets/Scripts/FloatingText.js
+++ New Unity Project/Assets/Scripts/FloatingText.js
@@ -2,31 +2,33 @@
 
 import UnityEngine.UI;
 
-public var storyPanel : Animator;
+// Commented out stuff relating to the animator because i couldn't 
+// wrap my head around setting up the animator
+//public var storyPanel : Animator;
 
 var storyText: Text = null;
 private var isHidden : boolean;
 
 function showStory() { 
-   storyPanel.enabled = true;
+   //storyPanel.enabled = true;
    gameObject.SetActive(true);
    //isHidden = storyPanel.GetBool("isHidden");
-   storyPanel.SetBool("isHidden", !isHidden);  
+   //storyPanel.SetBool("isHidden", !isHidden);  
 }
 
 function Hide() {
 //   isHidden = storyPanel.GetBool("isHidden");
-   storyPanel.SetBool("isHidden", isHidden); 
-   storyPanel.enabled = false;
+   //storyPanel.SetBool("isHidden", isHidden); 
+   //storyPanel.enabled = false;
    gameObject.SetActive(false); 
 }
 
 function Start () {
-   var transform : RectTransform = storyPanel.gameObject.transform as RectTransform;
+   //var transform : RectTransform = storyPanel.gameObject.transform as RectTransform;
    //var position : Vector2 = transform.anchoredPosition;
    //position.y -= transform.rect.height;
    //transform.anchoredPosition = position; 
-   storyPanel = GetComponent(Animator);
+   //storyPanel = GetComponent(Animator);
 }
 
 
