8321b8981003cfd6514a17daa92ec903654e4ca0
Dylan <Dylan@localhost>
2015-02-04 16:54:18 +1300

    Pretty much finished the boss

--- New Unity Project/Assets/Scripts/Boss.js
+++ New Unity Project/Assets/Scripts/Boss.js
@@ -1,30 +1,31 @@
 ﻿#pragma strict
 
-var maxSpeed : float = 10;
-var jumpForce : float;
-var crouchSpeed : float = .5;
-var airControl : boolean = true;
-var whatIsGround : LayerMask;
-
-static var health : float = 10;
+// Dodging stuff 
+var dodgeRandom : float;
 var dodgeColliderRight : EdgeCollider2D;
 var dodgeColliderLeft : EdgeCollider2D;
-var dodgeRandom : float;
-public var body : BoxCollider2D;
-public var legs : CircleCollider2D;
 var dodgeTimer: float;
 private var dodgeTimerLeft: float = 0;
-var facingRight : boolean = true;
+
+// Character stuff
+//var facingRight : boolean = true;
 private var groundCheck : Transform;
 private var groundedRadius : float = .2;
 var grounded : boolean = false;
 private var ceilingCheck : Transform;
 private var ceilingRadius : float = .01;
 private var anim : Animator;
+var whatIsGround : LayerMask;
+var maxSpeed : float = 10;
+var jumpForce : float;
+var crouchSpeed : float = .5;
+var airControl : boolean = true;
+
+// Health bar
+static var health : float = 10;
 var HealthBar : Scrollbar;
 var Health : float = 100;
-
-
+var dodgeEnabled : boolean = true;
 
 function Awake () {
 	anim = GetComponent(Animator);
@@ -32,32 +33,26 @@ function Awake () {
 	ceilingCheck = transform.Find("CeilingCheck");		
 }
 
-private function FixedUpdate () {/*
+private function FixedUpdate () {			
 	if(dodgeTimerLeft <= 0) {
 		dodgeRandom = Random.Range(0, 30);
 		if (dodgeRandom > 15) {
+			dodgeEnabled = true;
 			dodgeColliderRight.enabled = true;
 			dodgeColliderLeft.enabled = true;
 		} else {
+			dodgeEnabled = false;
 			dodgeColliderRight.enabled = false;
 			dodgeColliderLeft.enabled = false;
 		}
 		dodgeTimerLeft = dodgeTimer;		
-	}*//*
+	}
 	if (dodgeTimerLeft > 0) { 
 		dodgeTimerLeft -= Time.deltaTime;
-	}*/
+	}
 	grounded = Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround);
 	anim.SetBool("Ground", grounded);
 	anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
-	
-	/*if (facingRight) {
-		dodgeColliderLeft.enabled = false;
-		dodgeColliderRight.enabled = true;
-	} else if (!facingRight) {
-		dodgeColliderRight.enabled = false;	
-		dodgeColliderLeft.enabled = true;
-	}*/
 }
 
 function FireShoot() {
@@ -75,15 +70,14 @@ function IceShoot() {
 	iceAttack = GetComponent(AttackClassBoss);
 	iceAttack.IceShoot();      	
 }
-
+/*
 function Move (move : float, crouch : boolean, jump : boolean) {
 	if (!crouch && anim.GetBool("Crouch")) {
 		if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround)) {
 			crouch = true;
 		}
 	}
-	anim.SetBool("Crouch", crouch);
-	
+	anim.SetBool("Crouch", crouch);	
 	if (grounded || airControl) {
 		move = (crouch ? move*crouchSpeed : move);
 		
@@ -103,7 +97,7 @@ function Move (move : float, crouch : boolean, jump : boolean) {
 		rigidbody2D.AddForce(new Vector2(0, jumpForce));
 	}
 }
-
+*/
 function Dodge () {
 	if (grounded && anim.GetBool("Ground")) {
 		grounded = false;
@@ -114,15 +108,13 @@ function Dodge () {
 
 function OnTriggerEnter2D(other: Collider2D) {
 	if (other.tag == "Fireball") {
-		/*if (grounded && anim.GetBool("Ground")) {
-		//if(number > 4 && grounded == true && anim.GetBool("Ground")){
-			grounded = false;
-			anim.SetBool("Ground", false);
-			rigidbody2D.AddForce(new Vector2(0, jumpForce));
-		} else {*/
+		if (dodgeEnabled) {
+			Dodge();
+		} else {
 			Damage(10);
 			health--;
-		//}
+			Destroy(other.gameObject);
+		}
 	}
 }
 
@@ -130,7 +122,7 @@ function Damage(value : float) {
 	Health -= value;
 	HealthBar.size = Health / 100;
 }
-
+/*
 //Changes the direction the boss is facing
 private function Flip() {
 	facingRight = !facingRight;
@@ -138,3 +130,4 @@ private function Flip() {
 	theScale.x *= -1;
 	transform.localScale = theScale;	            
 }            
+*/
--- New Unity Project/Assets/Scripts/EnemyAI.js
+++ New Unity Project/Assets/Scripts/EnemyAI.js
@@ -5,17 +5,20 @@ private var enemy : GameObject;
 private var player : GameObject;
 private var boss : Boss;
 var maxSpeed : float = 10;
-private var facingRight : boolean = true;
+var facingRight : boolean = true;
 private var anim : Animator;
 var jumpForce : float = 400;
 var attackClass : AttackClassBoss;
 
-//Shot Timer
+// Random Attacks
+var randAttack : float;
+
+// Shot Timer
 var shootCooldownTime: float;
 var iceShootCooldownTime : float;
 private var shootCooldownTimeLeft: float = 0;
 private var iceShootCooldownTimeLeft : float = 0;
-//Big Attack Timer
+// Big Attack Timer
 private var bigAttackTimer : float = 5;
 private var iceAttackTimer : float = 5;
 
@@ -26,15 +29,12 @@ function Start () {
 	player = GameObject.FindGameObjectWithTag("Player");
 }  
 
-
 function BigAttack () {
 	gameObject.GetComponent(Transform).position.x = -8.73;
 	gameObject.GetComponent(Transform).position.y = 5.83;
 	if(shootCooldownTimeLeft <= 0) {
 		var i : int;
 		for (i = 0; i < 7; i++) {
-			//var bigAttack : AttackClassBoss;
-			//attackClass = GetComponent(AttackClassBoss);
 			attackClass.BigAttack();
 		}
 		shootCooldownTimeLeft = shootCooldownTime;		
@@ -49,8 +49,7 @@ function BigIceAttack () {
 	gameObject.GetComponent(Transform).position.y = 5.9;
 	if(iceShootCooldownTimeLeft <= 0) {
 		var i : int;
-		for (i = 0; i < 7; i++) {
-			//attackClass = GetComponent(AttackClassBoss);
+		for (i = 0; i < 5; i++) {
 			attackClass.BigIceAttack();
 		}
 		iceShootCooldownTimeLeft = iceShootCooldownTime;		
@@ -62,15 +61,17 @@ function BigIceAttack () {
 
 //Fixed Update
 function FixedUpdate () {
-	//If ready to attack
+	// When health gets low enough to special fire attack
 	if (Boss.health == 3 && bigAttackTimer > 0) {
 		BigAttack();
 		bigAttackTimer -= Time.deltaTime;
 	}
+	// When health gets low enough to special ice attack
 	if (Boss.health == 6 && iceAttackTimer > 0) {
 		BigIceAttack();
 		iceAttackTimer -= Time.deltaTime;
 	}
+	// When health is 0 just restart boss battle for now
 	if (Boss.health == 0) {
 		Application.LoadLevel("BossBattle");
 		Boss.health = 10;
@@ -81,16 +82,24 @@ function FixedUpdate () {
 			Flip();
 		}
 		rigidbody2D.velocity = new Vector2(-.3*maxSpeed, rigidbody2D.velocity.y);
+		randAttack = Random.Range(0,100);
+		if (randAttack == 1) {
+			attackClass.Shoot();
+		}
 	// if the player is to the right of the enemy, walk right
 	} else if (player.transform.position.x > gameObject.transform.position.x) {
 		if (!facingRight) {
 			Flip();
 		}
-		rigidbody2D.velocity = new Vector2(.3*maxSpeed, rigidbody2D.velocity.y);	
+		rigidbody2D.velocity = new Vector2(.3*maxSpeed, rigidbody2D.velocity.y);
+		randAttack = Random.Range(0,100);
+		if (randAttack == 1) {
+			attackClass.Shoot();
+		}	
 	}	
 }
 
-//Changes the direction the boss is facing
+// Changes the direction the boss is facing
 private function Flip() {
 	facingRight = !facingRight;
 	var theScale : Vector3 = transform.localScale;
