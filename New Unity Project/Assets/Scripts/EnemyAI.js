3323028dec0b5d3a66e5e8e8ef2e5e59d70aa42a
Dylan <Dylan@localhost>
2015-02-08 15:45:47 +1300

    changes to boss level

--- New Unity Project/Assets/Scripts/EnemyAI.js
+++ New Unity Project/Assets/Scripts/EnemyAI.js
@@ -30,8 +30,8 @@ function Start () {
 }  
 
 function BigAttack () {
-	gameObject.GetComponent(Transform).position.x = -11.29;
-	gameObject.GetComponent(Transform).position.y = 7.962;
+	gameObject.GetComponent(Transform).position.x = -15.8;
+	gameObject.GetComponent(Transform).position.y = 14;
 	if(shootCooldownTimeLeft <= 0) {
 		var i : int;
 		for (i = 0; i < 7; i++) {
@@ -45,8 +45,8 @@ function BigAttack () {
 }
 
 function BigIceAttack () {
-	gameObject.GetComponent(Transform).position.x = 10.33;
-	gameObject.GetComponent(Transform).position.y = 7.72;
+	gameObject.GetComponent(Transform).position.x = 15.5;
+	gameObject.GetComponent(Transform).position.y = 14;
 	if(iceShootCooldownTimeLeft <= 0) {
 		var i : int;
 		for (i = 0; i < 5; i++) {
