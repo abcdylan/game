43f01dcde363ace998737f1b47a967519bf3b511
Hannah <vanha241@student.otago.ac.nz>
2015-02-08 19:10:27 +1300

    edited the hud panel stuff ideally things should change if you get puzzle pieces and if you get the abilities

---
+++ New Unity Project/Assets/Scripts/RespawnPlayer.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+//var Player : GameObject;
+
+var spawnPoint : Transform;
+
+function OnTriggerEnter2D(other : Collider2D) {
+   //Destroy(other.gameObject);
+   //var P : GameObject;
+   //var P = Instantiate(Player);
+   //WaitForSeconds(3);
+   if(other.tag=="Player"){
+     other.transform.position = spawnPoint.position;
+   }
+   if(other.tag=="Enemy" || other.tag == "FallenGround"){
+     Destroy(other.gameObject);
+   }
+   //var cam = camera.main.getComponent(CameraController);
+   //camera.player = P.transform;
+   
+}
+
+/*
+function Start () {
+
+}
+
+function Update () {
+
+}*/
