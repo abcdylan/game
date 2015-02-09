103bc7da5f7bfb37c639f8a7342f7625a421740a
nmckellow <nmckellow@localhost>
2015-02-10 12:38:07 +1300

    merging with latest default

---
+++ EditUnityProject/Assets/SampleAssets/2D/Scripts/disableScripts/StoryManager.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+//import UnityEngine.UI;
+
+//display panel
+
+//var theStory : GUIText;
+var storyText : String = "change me";
+
+var displayMessage : boolean = false;
+var displayTime: float = 3.0;
+
+//on collision with a trigger
+function OnTriggerEnter2D(other : Collider2D) {
+   //check the tag of the object the 
+   //object has collided with
+   if (other.tag == "Player") {
+   // if collided with story object,
+   //set story flag to true
+      displayMessage = true;
+      yield WaitForSeconds(displayTime);
+      displayMessage = false;
+      }
+}
+
+/*function ShowStory() {
+   displayMessage = true;
+   gameObject.setActive(true);
+}
+
+function Hide() {
+*/
+
+
+function OnGui() {
+    if ( displayMessage ) {
+      GUI.Label(new Rect(Screen.width * 0.5f - 50f, Screen.height * 0.5f - 10f, 100f, 20f), storyText);
+   }
+}
+
+/*
+function Update () {
+   if(displayMessage && !Input.GetKeyDown(KeyCode.DownArrow)) {
+   
+   }
+}
+*/
---
+++ New Unity Project/Assets/Scripts/HealthBar.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var HealthBar : Scrollbar;
+var Health : float = 100;
+
+function Damage(value : float) {
+	Health -= value;
+	HealthBar.size = Health / 100;
+}
