c0c77da933dd4c4eaa2c49d4b7d47344b6076ca9
Hannah <vanha241@student.otago.ac.nz>
2015-02-10 16:47:18 +1300

    updated fire level

---
+++ EditUnityProject/Assets/SampleAssets/2D/Scripts/OffsetScroller.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var scrollSpeed: float;
+private var savedOffset: Vector2;
+
+function Start () {
+	savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+	
+}
+
+function Update () {
+	    var x: float =  Mathf.Repeat (Time.time * scrollSpeed, 1);
+        var offset : Vector2 = new Vector2 (x, savedOffset.y);
+        renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+    }
+
+function OnDisable () {
+        renderer.sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+}
---
+++ New Unity Project/Assets/Scripts/AttackClass.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+// variable storing projectile
+var fireballPrefab: Transform;
+var fireballPrefabLeft : Transform;
+var iceBlockPrefab: Transform;
+
+var shootCooldownTime: float;
+
+// keeping tabs on how much cooldown time is left
+private var shootCooldownTimeLeft: float = 0;
+private var character : Character;
+private var source : AudioSource;
+public var fireballCast : AudioClip;
+public var iceblockCast : AudioClip;
+
+private function Awake() {
+	character = GetComponent(Character);
+	source = GetComponent(AudioSource);
+	
+}
+
+//pre every frame
+function Update () {
+   //if there is still time left in cool down, reduce that time
+   // by the time since the last frame
+   if (shootCooldownTimeLeft > 0) {
+      shootCooldownTimeLeft -= Time.deltaTime;
+   }
+   
+}
+
+
+function IceShoot() {
+	if(shootCooldownTimeLeft <= 0) {
+		var iceBlock = Instantiate(iceBlockPrefab);
+		iceBlock.position = transform.position;
+		source.PlayOneShot(iceblockCast, 1f);
+		if(character.facingRight) {
+			iceBlock.position.x += 1;
+		} else {
+			iceBlock.position.x -= 1;
+		}
+		shootCooldownTimeLeft = shootCooldownTime;		
+	}
+}
+
+function Shoot() {
+	//shoot only if no cooldown time left
+	if (shootCooldownTimeLeft <= 0) {
+		if(character.facingRight) {
+			var fireball = Instantiate(fireballPrefab);
+			
+			// sets position of fireball 1 position in front of player
+			fireball.position = transform.position;
+			fireball.position.x += 1;
+			source.PlayOneShot(fireballCast, 1f);
+		} else {
+			var fireballLeft = Instantiate(fireballPrefabLeft);
+			// sets position of fireball 1 position in front of player
+			fireballLeft.position = transform.position;
+			fireballLeft.position.x -= 1;
+			source.PlayOneShot(fireballCast, 1f);
+		}     
+		// set time left until next shot to 
+		// the cooldown time
+     	shootCooldownTimeLeft = shootCooldownTime;
+	}
+}
