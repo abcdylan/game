58a0bb63fcd8510ca03cf99ba43d32ee8aab1222
nmckellow <nmckellow@localhost>
2015-02-10 15:25:23 +1300

    added sound to fantasy level

--- New Unity Project/Assets/Scripts/PickUpScript.js
+++ New Unity Project/Assets/Scripts/PickUpScript.js
@@ -2,9 +2,17 @@
 
 var pickUp : boolean = false;
 
+
+
+function Awake(){
+
+}
+
+
 function OnTriggerEnter2D (other : Collider2D) {
    if(other.tag == "Player") {
       pickUp = true;
+      audio.Play();
       gameObject.SetActive(false);
    //Destroy(gameObject);
    }
