ff477d81f1c0ac621304fa53c97c25e059897ef5
Hannah <vanha241@student.otago.ac.nz>
2015-02-11 14:43:30 +1300

    commit

---
+++ New Unity Project/Assets/Scripts/GameMaster.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(other: Collider2D) {
+	if (other.tag =="GoToIceLevel") {
+	Application.LoadLevel("Ice Level Demo");
+	}
+	 
+	if (other.tag =="GoToFireLevel") {
+	Application.LoadLevel("FireLevel");
+	} 
+}
