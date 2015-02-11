88b701519c4a8fee36f54109c59eea82e9296242
nmckellow <nmckellow@localhost>
2015-02-12 11:29:50 +1300

    updating to latest version

---
+++ New Unity Project/Assets/Scripts/HUDScript.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+// images that will let you know what puzzle pieces
+// the character has gained
+var getPuz1 : Image;
+var getPuz2 : Image;
+var getPuz3 : Image;
+
+// images that will let the player know what abilities they
+// have access to
+var ability1 : Image;
+var ability2 : Image;
+var ability3 : Image;
+
+
+
+var character : GameObject;
+// the "puzzle" objects we want access to
+var pickkUp : GameObject;
+var pickkUp2 : GameObject;
+var pickkUp3 : GameObject;
+
+//private var charControl : Character;
+// reference to the script attached to the puzzle objects
+private var pickedUp1: PickUpScript;
+private var pickedUp2 : PickUpScript;
+private var pickedUp3 : PickUpScript;
+
+function Start() {
+   //var objArray = GameObject.FindGameObjectsWithTag("puzzle");
+   //pickedUp1 = objArray[0].GetComponent(Remover);
+   pickedUp1 = pickkUp.GetComponent(PickUpScript);
+   pickedUp2 = pickkUp2.GetComponent(PickUpScript);
+   pickedUp3 = pickkUp3.GetComponent(PickUpScript);
+   //getPuz1.color.a = 0;
+   //charControl = character.GetComponent(Character);
+   
+}
+
+function Update() {
+   if(pickedUp1.pickUp) {
+      getPuz1.color.a = 255;
+      //getAbil1.text = "Gotten";
+   }
+   if(pickedUp2.pickUp) {
+      getPuz2.color.a = 255;
+   }
+   if(pickedUp3.pickUp) {
+      getPuz3.color.a = 255;
+   }
+   if(AbilityManager.abilityPickedUp) {
+      ability1.color.a = 255;
+   }
+   if(AbilityManager.iAbility) {
+      ability2.color.a = 255;
+   }
+   if(AbilityManager.fAbility) {
+      ability3.color.a = 255;
+   }
+}
+
+/*
+function gotAbil() {
+   getAbil1.text = "Got ability";
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}*/
---
+++ New Unity Project/Assets/Scripts/backupCharacter.js.js
@@ -0,0 +1,79 @@
+﻿#pragma strict
+
+var maxSpeed : float = 10;
+var jumpForce : float = 400;
+var crouchSpeed : float = .5;
+var airControl : boolean = true;
+var whatIsGround : LayerMask;
+
+
+private var doubleJumpCount : int = 1;
+private var maxAirJumpCount = 1;
+var airJumpCount : int = 0; //how many more times can the player jump
+private var facingRight : boolean = true;
+private var groundCheck : Transform;
+private var groundedRadius : float = .2;
+private var grounded : boolean = false;
+private var ceilingCheck : Transform;
+private var ceilingRadius : float = .01;
+private var anim : Animator;
+
+
+function Awake () {
+	anim = GetComponent(Animator);
+	groundCheck = transform.Find("GroundCheck");
+	ceilingCheck = transform.Find("CeilingCheck");		
+}
+
+function FixedUpdate () {
+
+	grounded = Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround);
+	anim.SetBool("Ground", grounded);
+	anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+	
+
+}
+
+function Move (move : float, crouch : boolean, jump : boolean) {
+	if (!crouch && anim.GetBool("Crouch")) {
+		if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround)) {
+			crouch = true;
+		}
+	}
+	anim.SetBool("Crouch", crouch);
+	
+	if (grounded || airControl) {
+		move = (crouch ? move*crouchSpeed : move);
+		
+		anim.SetFloat("Speed", Mathf.Abs(move));
+		rigidbody2D.velocity = new Vector2(move*maxSpeed, rigidbody2D.velocity.y);
+		
+		if (move > 0 && !facingRight) {
+			Flip();
+		} else if (move < 0 && facingRight) {
+			Flip();
+		}
+	}
+	
+	if (grounded && jump && anim.GetBool("Ground")) {
+		grounded = false;
+		anim.SetBool("Ground", false);
+		rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+	}
+	 else if (grounded == false && jump && doubleJumpCount == 1) {
+             rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+             doubleJumpCount = 0;
+                 }
+         if (grounded) {
+                         doubleJumpCount = 1;
+                 }
+     }
+
+
+private function Flip() {
+	facingRight = !facingRight;
+	var theScale : Vector3 = transform.localScale;
+	theScale.x *= -1;
+	transform.localScale = theScale;	            
+}  
