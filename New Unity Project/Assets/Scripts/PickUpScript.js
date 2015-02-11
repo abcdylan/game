6847ef6940f9ad8c263a6ff47a3fa13832fec7e0
lenzoburger <lenzoburger@localhost>
2015-02-11 16:53:27 +1300

    nananananana

--- New Unity Project/Assets/Scripts/PickUpScript.js
+++ New Unity Project/Assets/Scripts/PickUpScript.js
@@ -2,18 +2,11 @@
 
 var pickUp : boolean = false;
 
-
-
-function Awake(){
-
-}
-
-
 function OnTriggerEnter2D (other : Collider2D) {
    if(other.tag == "Player") {
       pickUp = true;
-      audio.Play();
       gameObject.SetActive(false);
+      audio.Play();     
    //Destroy(gameObject);
    }
 }
