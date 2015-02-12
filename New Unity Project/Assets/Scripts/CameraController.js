96098b8b047508b6255dfcad2a162fc5650170ff
Hannah <vanha241@student.otago.ac.nz>
2015-02-12 15:50:58 +1300

    made small adjustments to enemy script since fireball attackes weren't killing them. changed sprites for falling ice spikes in lava level, changed background, added slide transition to boss level

---
+++ New Unity Project/Assets/Scripts/CameraController.js
@@ -0,0 +1,67 @@
+﻿#pragma strict
+
+// responsible for three things, following the character,
+// support a margin, and never allow the player to see outside of
+// the bounds we define
+
+//if we wanted to start the camera
+//view at the end of the level and track back to the
+//player 
+
+//var character : GameObject;
+//var Bozz : GameObject;
+
+var player : Transform;
+
+var Margin : Vector2;
+
+var smoothing : Vector2;
+
+public var Bounding : BoxCollider2D;
+public var isFollowing : boolean;
+
+private var min : Vector3;
+private var max : Vector3;
+
+function Start () {
+   min = Bounding.bounds.min;
+   max = Bounding.bounds.max;
+   isFollowing = true;
+   //player = Bozz.transform;
+   //Switch();
+
+}
+
+function Update () {
+   //var x : int;
+   //var y : int;
+   var x = transform.position.x;
+   var y = transform.position.y;
+   
+   if (isFollowing) {
+      if(Mathf.Abs(x - player.position.x) > Margin.x) {
+         x = Mathf.Lerp(x, player.position.x, smoothing.x * Time.deltaTime);
+      }
+      
+      if (Mathf.Abs(y - player.position.y) > Margin.y) {
+         y = Mathf.Lerp(y, player.position.y, smoothing.y * Time.deltaTime);
+      }
+      
+      var cameraHalfWidth = camera.orthographicSize * (Screen.width / Screen.height);
+      
+      x = Mathf.Clamp(x, min.x + camera.orthographicSize, max.x - camera.orthographicSize);
+      y = Mathf.Clamp(y, min.y + camera.orthographicSize, max.y - camera.orthographicSize);
+ 
+      transform.position = new Vector3(x, y, transform.position.z);
+           
+   }
+}
+
+/*
+function Switch() {
+   yield WaitForSeconds(3);
+   Bozz.particleSystem.Emit(200);
+   yield WaitForSeconds(1);
+   Bozz.SetActive(false);
+   player = character.transform;
+}*/
---
+++ New Unity Project/Assets/Scripts/GameMaster.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(other: Collider2D) {
+	if (other.tag =="GoToIceLevel") {
+	Application.LoadLevel("Ice Level Demo");
+	}
+	 
+	if (other.tag =="GoToFireLevel") {
+	Application.LoadLevel("FireLevel");
+	} 
+    
+    if (other.tag =="BossLevelTransition") {
+	Application.LoadLevel("BossBattle");
+	} 
+}
