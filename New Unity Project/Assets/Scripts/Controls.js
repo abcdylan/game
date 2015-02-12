0d52a9da28bc8715048ca734053e9efb17cac913
Dylan <Dylan@localhost>
2015-02-12 14:20:30 +1300

    mergin'

---
+++ New Unity Project/Assets/Scripts/Controls.js
@@ -0,0 +1,49 @@
+#pragma strict
+
+private var character : Character;
+private var jump : boolean;
+// fire ability boolean, whether or not the character has gotten it
+//private var fAbility : boolean = false;
+// ice ability boolean, whether or not the player has gotten it
+//private var iAbility : boolean = false;
+
+private function Awake() {
+	character = GetComponent(Character);
+}
+
+function OnTriggerEnter2D(other : Collider2D) {
+	if(other.tag == "disableAirControl") {
+		character.airControl = false;
+	} else if (other.tag == "enableAirControl") {
+		character.airControl = true;
+	}
+	//if colliding with the fire ability
+	if(other.tag == "FireAbility") {
+		AbilityManager.fAbility = true;
+		Destroy(other.gameObject);
+	}//if colliding with the fire ability
+	else if(other.tag == "IceAbility") {
+		AbilityManager.iAbility = true;
+		Destroy(other.gameObject);
+	}
+}
+
+private function Update() {
+	if (!jump) {
+		jump = Input.GetButtonDown("Jump");
+	}		
+	if(AbilityManager.fAbility && Input.GetKeyDown(KeyCode.Alpha1)) {
+	   character.FireShoot(); 
+    }
+    if(AbilityManager.iAbility && Input.GetKeyDown(KeyCode.Alpha2)) {
+       character.IceShoot();
+    }
+	
+}
+
+private function FixedUpdate() {
+	var crouch : boolean = Input.GetKey(KeyCode.LeftControl);
+	var h : float = Input.GetAxis("Horizontal");
+	character.Move(h, crouch, jump);
+	jump = false;
+}
---
+++ New Unity Project/Assets/Scripts/PickUpScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var pickUp : boolean = false;
+
+
+
+function Awake(){
+
+}
+
+
+function OnTriggerEnter2D (other : Collider2D) {
+   if(other.tag == "Player") {
+      pickUp = true;
+      audio.Play();
+      gameObject.SetActive(false);
+   //Destroy(gameObject);
+   }
+}
+
+/*
+function Start () {
+
+}
+
+function Update () {
+
+}*/
