7f1e27d7ba70687601d36ac114fab5f760fadca5
lenzoburger <lenzoburger@localhost>
2015-02-13 18:15:17 +1300

    cleaning up

---
+++ Dream Team Project/Assets/Scripts/WaitScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+//wait for a period of seconds before
+// loading next scene
+
+public var Level : String;
+public var wait : int;
+
+function Start () {
+   yield WaitForSeconds(wait);
+   Application.LoadLevel(Level);
+}
