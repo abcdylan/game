ce41457b232b0f52d4c2513342b91cfd97c8d1a2
Hannah <vanha241@student.otago.ac.nz>
2015-02-13 15:00:12 +1300

    commiting changes i made to the healthy heart UI

--- New Unity Project/Assets/Scripts/Hearts.js
+++ New Unity Project/Assets/Scripts/Hearts.js
@@ -1,19 +1,24 @@
 ﻿#pragma strict
-var heart1 : GameObject;
-var heart2 : GameObject;
-var heart3 : GameObject;
+var heart1 : Image;
+var heart2 : Image;
+var heart3 : Image;
+
+/*
+function Start() {
+   
+}*/
 
 function Update () {
  if(Character.charHealth==3){
-    heart1.GetComponent(SpriteRenderer).color.a =1;
-    heart2.GetComponent(SpriteRenderer).color.a =1;
-    heart3.GetComponent(SpriteRenderer).color.a =1;
+       heart1.color.a =1;
+       heart2.color.a =1;
+       heart3.color.a =1;
     }else if(Character.charHealth==2){
-    heart3.GetComponent(SpriteRenderer).color.a =0;    
+       heart3.color.a =0;    
     }else if(Character.charHealth==1){  
-    heart2.GetComponent(SpriteRenderer).color.a =0;       
+       heart2.color.a =0;       
     } else if(Character.charHealth==0){  
-    heart1.GetComponent(SpriteRenderer).color.a =0;       
+       heart1.color.a =0;       
     }  
 
 }
